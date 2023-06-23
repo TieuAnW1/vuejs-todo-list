c271441cf8d781c182a374eb54ff4bd26b5d4114
TiuAn <hoangson123654@gmail.com>
2023-06-23 15:38:53 +0700

    Display todos data from localStorage

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
 	<div class="wrapper">
 		<div class="content">
 			<addTodo></addTodo>
+			<listTodo></listTodo>
 		</div>
 	</div>
 </template>
 
 <script>
 import addTodo from './components/addTodo/index.vue';
+import listTodo from './components/listTodo/index.vue';
 
-export default { components: { addTodo } };
+export default { components: { addTodo, listTodo } };
 </script>
---
+++ src/components/listTodo/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="listTodo">
+		<div v-if="todos.length === 0">No todo</div>
+		<div v-else>
+			<div v-for="(todo, index) in todos" :key="todo + index"> {{ todo.name }} + {{ todo.deadline }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'listTodo',
+	data() {
+		return {
+			todos: JSON.parse(localStorage.getItem('todos')) || [],
+		};
+	},
+};
+</script>
