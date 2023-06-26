ee535f3b56d7ee55e9720e41c3260c1603850d49
TiuAn <hoangson123654@gmail.com>
2023-06-26 23:05:11 +0700

    Add en.json for multi language

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n';
 import vietnameseLanguage from './locales/vi.json';
+import englishLanguage from './locales/en.json';
 import { Text } from './constants';
 
 const i18n = createI18n({
 	locale: Text.language.en,
 	messages: {
+		en: englishLanguage,
 		vi: vietnameseLanguage,
 	},
 });
